636a0c86fb0f89bb5f259edff194c44c7d3c9589
M4ss1ck <4ndyraul@gmail.com>
2022-07-27 09:17:32 -0400

    add react-helmet

--- frontend/gatsby-config.ts
+++ frontend/gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    author: `Massick`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -11,6 +11,7 @@ const config: GatsbyConfig = {
   plugins: [
     `gatsby-plugin-pnpm`,
     `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
   ],
 }
 
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react"
+import { Helmet } from "react-helmet"
 import Interface from "../components/Interface"
 
 const IndexPage = () => {
   return (
-    <main className="container mx-auto flex flex-col items-center text-center">
+    <main className="container flex flex-col items-center mx-auto text-center">
+      <Helmet title="TODO app" />
       <Interface />
     </main>
   )
