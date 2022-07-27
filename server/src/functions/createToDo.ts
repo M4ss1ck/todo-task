cd7676051834e229d702ed822a09023b50c6a180
M4ss1ck <4ndyraul@gmail.com>
2022-07-27 13:53:22 -0400

    set id in createToDo function

--- server/src/functions/createToDo.ts
+++ server/src/functions/createToDo.ts
@@ -3,8 +3,9 @@ import TODO from "../models/ToDo"
 
 export const createToDo = async (req: Request, res: Response) => {
     const { status, task } = req.body;
+    const { max } = (await TODO.query().max("id") as any[])[0]
     const newToDo = await TODO.query().insert({
-        status, task
+        status, task, id: max + 1
     })
     res.json(newToDo)
 }
