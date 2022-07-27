7cc1a589c65dc063788dbafb79c099482c211e0a
M4ss1ck <4ndyraul@gmail.com>
2022-07-27 08:50:21 -0400

    add Themes

---
+++ frontend/src/components/Themes.tsx
@@ -0,0 +1,138 @@
+import React, { useEffect } from "react"
+import { Listbox, Transition } from "@headlessui/react"
+
+const daisyUIThemes = [
+  "light",
+  "dark",
+  "cupcake",
+  "bumblebee",
+  "emerald",
+  "corporate",
+  "synthwave",
+  "retro",
+  "cyberpunk",
+  "valentine",
+  "halloween",
+  "garden",
+  "forest",
+  "aqua",
+  "lofi",
+  "pastel",
+  "fantasy",
+  "wireframe",
+  "black",
+  "luxury",
+  "dracula",
+  "cmyk",
+  "autumn",
+  "business",
+  "acid",
+  "lemonade",
+  "night",
+  "coffee",
+  "winter",
+]
+
+const Themes: React.FC = () => {
+  let initialTheme = ""
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme")
+    if (theme != undefined && theme != "") {
+      document.documentElement.setAttribute("data-theme", theme)
+      initialTheme = theme
+    } else {
+      initialTheme =
+        document.documentElement.getAttribute("data-theme") || "light"
+    }
+  }, [])
+
+  const [theme, setTheme] = React.useState(initialTheme)
+
+  const handleChange = (theme: string) => {
+    setTheme(theme)
+    document.documentElement.classList.add("theme-transition")
+    document.documentElement.setAttribute("data-theme", theme)
+    localStorage.setItem("theme", theme)
+    window.setTimeout(function () {
+      document.documentElement.classList.remove("theme-transition")
+    }, 1000)
+  }
+
+  return (
+    <div className="relative z-50 flex flex-row items-center justify-center my-auto bg-opacity-100">
+      <Listbox value={theme} onChange={handleChange}>
+        <div className="relative">
+          <Listbox.Label hidden>Select a theme</Listbox.Label>
+          <Listbox.Button as={React.Fragment}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-8 h-8 cursor-pointer focus:outline-none"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4 2a2 2 0 00-2 2v11a3 3 0 106 0V4a2 2 0 00-2-2H4zm1 14a1 1 0 100-2 1 1 0 000 2zm5-1.757l4.9-4.9a2 2 0 000-2.828L13.485 5.1a2 2 0 00-2.828 0L10 5.757v8.486zM16 18H9.071l6-6H16a2 2 0 012 2v2a2 2 0 01-2 2z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Listbox.Button>
+          <Transition
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <Listbox.Options
+              className="absolute top-0 right-0 block overflow-y-auto rounded-lg h-72 scrollbar-thin scrollbar-thumb-current scrollbar-track-transparent"
+              tabIndex={0}
+            >
+              {daisyUIThemes.map(currentTheme => (
+                <Listbox.Option
+                  key={currentTheme}
+                  value={currentTheme}
+                  // disabled={currentTheme.value === theme}
+                  as={React.Fragment}
+                >
+                  {({ active, selected }) => (
+                    <li
+                      data-theme={currentTheme}
+                      className={`relative cursor-default flex-nowrap w-full select-none py-2 px-2 flex flex-row items-center justify-end`}
+                    >
+                      {selected && (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="inline-flex w-6 h-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth={2}
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+                      )}
+                      {currentTheme}{" "}
+                      <span className="p-1 m-1 ml-2 rounded-full bg-primary"></span>
+                      <span className="p-1 m-1 rounded-full bg-secondary"></span>
+                      <span className="p-1 m-1 rounded-full bg-accent"></span>
+                    </li>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  )
+}
+
+export default Themes
--- frontend/src/components/ToDoList.tsx
+++ frontend/src/components/ToDoList.tsx
@@ -2,27 +2,31 @@ import React from 'react'
 import ToDoItem from './ToDoItem'
 import { useToDoContext } from '../context/ToDoContext'
 import { Add } from './Buttons'
+import Themes from './Themes'
 
 const ToDoList = () => {
     const { todos, changeView } = useToDoContext()
-    return <div className='min-h-screen flex flex-col items-center md:text-xl'>
-        <section className='flex flex-row'>
-            <h1 className='header'>My ToDo list</h1>
-            <div className="dropdown dropdown-end mt-4">
-                <label tabIndex={0} className="btn btn-circle btn-ghost btn-xs text-info">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                </label>
-                <div tabIndex={0} className="card compact dropdown-content shadow bg-base-100 rounded-box w-64 border border-content">
-                    <div className="card-body">
-                        <h2 className="card-title">This is the ToDo list</h2>
-                        <p>Tap the pencil icon to edit a ToDo or the trash bin to delete it</p>
+    return <div className='flex flex-col items-center min-h-screen md:text-xl'>
+        <section className='flex flex-row items-center justify-between w-full'>
+            <div className='flex flex-row mx-auto'>
+                <h1 className='header'>My ToDo list</h1>
+                <div className="mt-4 dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-circle btn-ghost btn-xs text-info">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    </label>
+                    <div tabIndex={0} className="w-64 border shadow card compact dropdown-content bg-base-100 rounded-box border-content">
+                        <div className="card-body">
+                            <h2 className="card-title">This is the ToDo list</h2>
+                            <p>Tap the pencil icon to edit a ToDo or the trash bin to delete it</p>
+                        </div>
                     </div>
                 </div>
             </div>
+            <Themes />
         </section>
-        <ul className='my-4 flex flex-col items-center justify-center'>
+        <ul className='flex flex-col items-center justify-center my-4'>
             {todos.map((todo) => todo.id && <ToDoItem key={todo.id} id={todo.id} todo={todo} />)}
-            <li className='item btn btn-sm btn-ghost h-10' onClick={() => changeView("AddToDo")}><Add /></li>
+            <li className='h-10 item btn btn-sm btn-ghost' onClick={() => changeView("AddToDo")}><Add /></li>
         </ul>
     </div>
 }
