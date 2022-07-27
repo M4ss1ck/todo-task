742986df7196b72c12dc6b7df5e0a0796972bb2d
M4ss1ck <4ndyraul@gmail.com>
2022-07-27 09:17:16 -0400

    fix svg properties

--- frontend/src/components/ToDoList.tsx
+++ frontend/src/components/ToDoList.tsx
@@ -12,7 +12,7 @@ const ToDoList = () => {
                 <h1 className='header'>My ToDo list</h1>
                 <div className="mt-4 dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-circle btn-ghost btn-xs text-info">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                     </label>
                     <div tabIndex={0} className="w-64 border shadow card compact dropdown-content bg-base-100 rounded-box border-content">
                         <div className="text-left card-body">
